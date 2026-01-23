1170d5f9d810c2b2c61e4a4d2f88763b3a92eb57
Sannabs <sannabsjammeh54@gmail.com>
2026-01-23 00:02:48 +0000

    Integrate authentication middleware for user retrieval route
    - Added requireAuth middleware to the /me route to ensure that only authenticated users can access their information, enhancing security and access control.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,9 +3,10 @@ import {
   tenantSignUp,
   getMe,
 } from "../controllers/auth.controller.js";
+import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", tenantSignUp);
-router.get("/me", getMe);
+router.get("/me", requireAuth, getMe);
 export default router;
