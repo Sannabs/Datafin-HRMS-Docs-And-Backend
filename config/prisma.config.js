a5b8436a6c0bc5ff232c570e0f99b8f135bcd816
Sannabs <sannabsjammeh54@gmail.com>
2026-03-29 15:58:25 +0000

    n

--- config/prisma.config.js
+++ config/prisma.config.js
@@ -2,24 +2,21 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import pg from "pg";
-import fs from "fs";
 
-const connectionString = process.env.DATABASE_URL;
+let connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
-const isProduction = process.env.NODE_ENV === "production";
+// Strip query params - we handle SSL via pool config
+connectionString = connectionString.split('?')[0];
 
 const pool = new pg.Pool({
   connectionString,
-  ...(isProduction && {
-    ssl: {
-      rejectUnauthorized: true,
-      ca: fs.readFileSync("/var/www/backend/ca-certificate.crt").toString(),
-    },
-  }),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const adapter = new PrismaPg(pool);
