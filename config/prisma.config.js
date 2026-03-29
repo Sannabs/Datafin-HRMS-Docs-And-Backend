f3f48ffed0c2c5b0f34fae5bfc8a6981c144b139
Sannabs <sannabsjammeh54@gmail.com>
2026-03-29 15:39:06 +0000

    ca-cert

--- config/prisma.config.js
+++ config/prisma.config.js
@@ -2,13 +2,26 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import pg from "pg";
+import fs from "fs";
+
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
-const pool = new pg.Pool({ connectionString });
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new pg.Pool({
+  connectionString,
+  ...(isProduction && {
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync("/var/www/backend/ca-certificate.crt").toString(),
+    },
+  }),
+});
+
 const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
